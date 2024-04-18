6d7032323ea7cafe5df32a95c850ff2fd7c8e7cd
Camilo <camilohi.1196@gmail.com>
2024-04-17 20:17:22 -0500

    Ejercicio 1 Finish

--- Ejercicio1/E1.js
+++ Ejercicio1/E1.js
@@ -1,4 +1,5 @@
-let frase = "Hola Mundo";
-console.log(frase);
-let cortar=frase.length("");
-console.log(cortar)
+let frase = "hola";
+let primeraLetra = frase[0].toUpperCase();
+let resto = frase.slice(1,3);
+let ultimaLetra = frase[3].toUpperCase();
+console.log(primeraLetra+resto+ultimaLetra);
